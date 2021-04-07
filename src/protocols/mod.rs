36f7dfd32d26cdbe831338f07d169d044076aa18
Arend van Beelen jr <arendjr@gmail.com>
2021-04-07 15:44:03 +0200

    PR feedback

--- src/protocols/mod.rs
+++ src/protocols/mod.rs
@@ -1,4 +1,5 @@
 pub mod core;
+pub mod notebook;
 pub mod notebook_state;
 pub mod operations;
 pub mod realtime;
--- src/protocols/notebook.rs
+++ src/protocols/notebook.rs
@@ -1,11 +1,13 @@
 use crate::protocols::core::{Cell, TimeRange};
+use serde::{Deserialize, Serialize};
 
 /// A notebook with all (meta)data included.
-#[derive(Debug, Deserialize, Serialize, Clone)]
+#[derive(Debug, Deserialize, Serialize, PartialEq, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct Notebook {
     pub id: String,
     pub cells: Vec<Cell>,
+    pub revision: usize,
     pub time_range: TimeRange,
     pub title: String,
 }
