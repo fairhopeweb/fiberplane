de68ccfe9a7e8419887cd652823c9c2b4fbe216a
Evan Schwartz <3262610+emschwartz@users.noreply.github.com>
2022-01-06 09:17:32 -0500

    Add base64uuid crate (#62)

---
+++ base64uuid/src/lib.rs
@@ -0,0 +1,135 @@
+use base64::DecodeError;
+use serde::{Deserialize, Serialize};
+use std::convert::TryFrom;
+use std::fmt::Debug;
+use std::{fmt::Display, str::FromStr};
+use thiserror::Error;
+use uuid::Uuid;
+
+#[cfg(test)]
+mod tests;
+
+/// A Base64Uuid is a base64url-encoded UUID that is
+/// used for IDs across Fiberplane's products.
+/// The main motivation is that it is a slightly more
+/// condensed representation of a UUID and it looks
+/// better in URLs.
+#[derive(PartialEq, Hash, Eq, Serialize, Deserialize, Clone, Copy)]
+// Force serde to go through the String representation to ensure
+// it is (de)serialized in the base64url-encoded format
+#[serde(try_from = "&str", into = "String")]
+pub struct Base64Uuid(Uuid);
+
+impl Base64Uuid {
+    pub fn new() -> Self {
+        let uuid = Uuid::new_v4();
+        Base64Uuid(uuid)
+    }
+
+    pub fn nil() -> Self {
+        Base64Uuid(Uuid::nil())
+    }
+
+    pub fn as_uuid(&self) -> &Uuid {
+        &self.0
+    }
+
+    pub fn to_uuid(self) -> Uuid {
+        self.0
+    }
+
+    pub fn into_uuid(self) -> Uuid {
+        self.0
+    }
+
+    /// Parse an ID either from the normal UUID string representation
+    /// or from the base64url-encoded representation
+    pub fn parse_str(s: &str) -> Result<Self, InvalidId> {
+        let uuid = if s.len() == 22 {
+            // This will return an error if the string is invalid
+            let bytes = decode_base64(s).map_err(|_| InvalidId)?;
+            Uuid::from_slice(&bytes).map_err(|_| InvalidId)?
+        } else if s.len() == 36 {
+            Uuid::from_str(s).map_err(|_| InvalidId)?
+        } else {
+            return Err(InvalidId);
+        };
+
+        Ok(Base64Uuid(uuid))
+    }
+}
+
+impl AsRef<Uuid> for Base64Uuid {
+    fn as_ref(&self) -> &Uuid {
+        &self.0
+    }
+}
+
+impl From<Uuid> for Base64Uuid {
+    fn from(uuid: Uuid) -> Self {
+        Base64Uuid(uuid)
+    }
+}
+
+impl From<Base64Uuid> for Uuid {
+    fn from(base64: Base64Uuid) -> Self {
+        base64.into_uuid()
+    }
+}
+
+impl From<Base64Uuid> for String {
+    fn from(uuid: Base64Uuid) -> Self {
+        uuid.to_string()
+    }
+}
+
+impl FromStr for Base64Uuid {
+    type Err = InvalidId;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Base64Uuid::parse_str(s)
+    }
+}
+
+impl TryFrom<&str> for Base64Uuid {
+    type Error = InvalidId;
+
+    fn try_from(s: &str) -> Result<Self, Self::Error> {
+        Base64Uuid::parse_str(s)
+    }
+}
+
+impl Display for Base64Uuid {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&encode_base64(&self.0.as_bytes()))
+    }
+}
+
+impl Debug for Base64Uuid {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&encode_base64(&self.0.as_bytes()))
+    }
+}
+
+impl Default for Base64Uuid {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+#[derive(Debug, Error)]
+#[error("Invalid Base64Uuid")]
+pub struct InvalidId;
+
+/// Encode input to a base64 string using the UrlSafe characters set and no
+/// padding.
+fn encode_base64<T: AsRef<[u8]>>(input: T) -> String {
+    let config = base64::Config::new(base64::CharacterSet::UrlSafe, false);
+    base64::encode_config(input, config)
+}
+
+/// Decode a base64 input with the UrlSafe character set and no padding.
+fn decode_base64<T: AsRef<[u8]>>(input: T) -> Result<Vec<u8>, DecodeError> {
+    let config = base64::Config::new(base64::CharacterSet::UrlSafe, false);
+    base64::decode_config(input, config)
+}
---
+++ base64uuid/src/tests.rs
@@ -0,0 +1,60 @@
+use super::*;
+use serde_json;
+
+#[test]
+fn from_uuid() {
+    let known_id = Uuid::parse_str("b0c1ee86-6f46-4f1b-8d8b-7849e75dbcee").unwrap();
+    let as_b64 = Base64Uuid::from(known_id);
+    assert_eq!(as_b64.to_string(), "sMHuhm9GTxuNi3hJ51287g");
+}
+
+#[test]
+fn from_str() {
+    let id = Base64Uuid::parse_str("sMHuhm9GTxuNi3hJ51287g").unwrap();
+    assert_eq!(id.to_string(), "sMHuhm9GTxuNi3hJ51287g");
+
+    let id = Base64Uuid::parse_str("b0c1ee86-6f46-4f1b-8d8b-7849e75dbcee").unwrap();
+    assert_eq!(id.to_string(), "sMHuhm9GTxuNi3hJ51287g");
+}
+
+#[test]
+fn as_uuid() {
+    let id = Base64Uuid::parse_str("sMHuhm9GTxuNi3hJ51287g").unwrap();
+    assert_eq!(
+        id.as_uuid(),
+        &Uuid::parse_str("b0c1ee86-6f46-4f1b-8d8b-7849e75dbcee").unwrap()
+    );
+}
+
+#[test]
+fn serialization() {
+    #[derive(Serialize)]
+    struct Person {
+        id: Base64Uuid,
+    }
+
+    let person = Person {
+        id: Base64Uuid::parse_str("b0c1ee86-6f46-4f1b-8d8b-7849e75dbcee").unwrap(),
+    };
+    let string = serde_json::to_string(&person).unwrap();
+    assert_eq!(string, "{\"id\":\"sMHuhm9GTxuNi3hJ51287g\"}");
+}
+
+#[test]
+fn deserialization() {
+    #[derive(Deserialize)]
+    struct Person {
+        id: Base64Uuid,
+    }
+
+    let string = "{\"id\":\"sMHuhm9GTxuNi3hJ51287g\"}";
+    let person: Person = serde_json::from_str(string).unwrap();
+    assert_eq!(person.id.to_string(), "sMHuhm9GTxuNi3hJ51287g");
+}
+
+#[test]
+fn debug_format() {
+    let uuid = Base64Uuid::parse_str("b0c1ee86-6f46-4f1b-8d8b-7849e75dbcee").unwrap();
+    let debug = format!("{:?}", uuid);
+    assert_eq!(&debug, "sMHuhm9GTxuNi3hJ51287g")
+}
