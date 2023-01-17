8a4cff19b8f177da388167c2e9c2d57cbd34aaaa
Arend van Beelen jr <arendjr@gmail.com>
2023-01-17 17:40:32 +0100

    Add constants for events query type and MIME type (#315)

--- fiberplane-models/src/providers/constants.rs
+++ fiberplane-models/src/providers/constants.rs
@@ -1,8 +1,10 @@
+pub const EVENTS_QUERY_TYPE: &str = "events";
 pub const STATUS_QUERY_TYPE: &str = "status";
 pub const SUGGESTIONS_QUERY_TYPE: &str = "suggestions";
 pub const TIMESERIES_QUERY_TYPE: &str = "timeseries";
 
 pub const CELLS_MIME_TYPE: &str = "application/vnd.fiberplane.cells";
+pub const EVENTS_MIME_TYPE: &str = "application/vnd.fiberplane.events";
 pub const FORM_ENCODED_MIME_TYPE: &str = "application/x-www-form-urlencoded";
 pub const STATUS_MIME_TYPE: &str = "application/vnd.fiberplane.provider-status";
 pub const SUGGESTIONS_MIME_TYPE: &str = "application/vnd.fiberplane.suggestions";
